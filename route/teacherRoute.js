608d6003508433a0321b4d106f34b49963493da2
DinhVanNgoc <39309110+Zephyssss@users.noreply.github.com>
2020-08-10 01:53:06 +0700

    get require teacher, update, delete teacher

--- route/teacherRoute.js
+++ route/teacherRoute.js
@@ -1,7 +1,8 @@
 const router = require("express").Router();
-const xlxs = require("xlsx");
+const xlsx = require("xlsx");
 
 const Teacher = require("../model/Teacher");
+const Subject = require("../model/Subject");
 const validate = require("../verify/validateTeacher");
 
 //CREATE NEW TEACHER
@@ -9,16 +10,21 @@ router.post("/", async (req, res) => {
   //validate data before save teacher
   const valid = await validate.createTeacherValidation({ ...req.body });
   if (valid.error)
-    return res
-      .status(400)
-      .json({ error: "Bad request", message: valid.error.details[0].message });
+    return res.status(400).json({ error: "Bad request", message: valid.error.details[0].message });
 
   //check teacher id exist yet
   const find = await Teacher.findOne({ teacher_id: req.body.teacher_id });
   if (find)
-    return res
-      .status(409)
-      .json({ error: "Conflict", message: "Teacher_id exist" });
+    return res.status(409).json({ error: "Conflict", message: "Teacher_id exist" });
+
+  //check grade subject
+  const checkGradeSubject = await Subject.findOne({
+    id_user: req.user._id,
+    sortName: req.body.subject.trim(),
+    grade: req.body.grade,
+  });
+  if(!checkGradeSubject)
+    return res.status(409).json({error:"Conflict", message:"Grade-Subject don't exist"})
 
   //init teacher
   const teacher = new Teacher({ id_user: req.user._id, ...req.body });
@@ -41,6 +47,13 @@ router.get("/", async (req, res) => {
   }
 });
 
+//GET LIST REQUIRE
+router.get("/requires", async (req, res) => {
+  const excel = await xlsx.readFile("./data/data.xlsx");
+  const data = xlsx.utils.sheet_to_json(excel.Sheets["require_teacher"]);
+  res.status(200).json({ total: data.length, data: data });
+});
+
 //GET SPECIAL TEACHER
 router.get("/:id", async (req, res) => {
   const valid = validate.idTeacherValidation({ id: req.params.id });
@@ -48,11 +61,76 @@ router.get("/:id", async (req, res) => {
     return res.status(400).json({ error: "Bad request", message: valid.error.details[0].message });
 
   try {
-    const findTeacher = await Teacher.findById(req.params.id);
+    const findTeacher = await Teacher.findOne({ _id: req.params.id });
+    if (!findTeacher)
+      res.status(404).json({ error: "Not found", message: "Teacher not found" });
     res.status(200).json(findTeacher);
   } catch (err) {
     res.status(400).json({ error: "Bad request", message: err });
   }
 });
+
+router.put("/:id", async (req, res) => {
+  //Validate data before update
+  const valid = await validate.updateTeacherValidation({id: req.params.id,...req.body,});
+  if (valid.error)
+    return res.status(400).json({ error: "Bad request", message: valid.error.details[0].message });
+
+  //Find teacher
+  const findTeacher = await Teacher.findOne({ _id: req.params.id });
+  if (!findTeacher)
+    return res.status(404).json({ error: "Not found", message: "Teacher not found" });
+
+  //Check teacher_id
+  if (req.body.teacher_id != undefined) {
+    if (req.body.teacher_id.trim() != findTeacher.teacher_id.trim()) {
+      const find = await Teacher.findOne({id_user: req.user._id,teacher_id: req.body.teacher_id.trim(),});
+      if (find)
+        return res.status(409).json({ error: "Conflict", message: "Teacher_id is exist" });
+      findTeacher.teacher_id = req.body.teacher_id.trim();
+    }
+  }
+
+  //update
+  if (req.body.name != undefined) findTeacher.name = req.body.name;
+  if (req.body.period_per_week != undefined)
+    findTeacher.period_per_week = req.body.period_per_week;
+  if (req.body.grade != undefined) findTeacher.grade = req.body.grade;
+  if (req.body.subject != undefined) findTeacher.subject = req.body.subject.trim();
+  if (req.body.require != undefined) findTeacher.require = req.body.require;
+
+  //check grade subject
+  const checkGradeSubject = await Subject.findOne({
+    id_user: req.user._id,
+    sortName: findTeacher.subject,
+    grade: findTeacher.grade,
+  });
+  if(!checkGradeSubject)
+    return res.status(409).json({error:"Conflict", message:"Grade-Subject don't exist"})
+
+  //update on database
+  try {
+    const update = await Teacher.findByIdAndUpdate(req.params.id, findTeacher);
+    res.status(201).json(findTeacher);
+  } catch (err) {
+    res.status(400).json({ error: "Bad request", message: err });
+  }
+});
+
+router.delete("/:id",async (req,res)=>{
+  const valid= await validate.idTeacherValidation({id: req.params.id});
+  if(valid.error)
+    return res.status(400).json({error:"Bad request",message:valid.error.details[0].message});
+
+  try{
+    const del = await Teacher.findByIdAndDelete(req.params.id);
+    if(!del)
+      res.status(404).json({error:"Not found",message:"Teacher not found"});
+    res.status(200).json(del);
+
+  }catch(err){
+    res.status(400).json({error:"Bad request",message:err});
+  }
+})
 
 module.exports = router;
--- verify/validateTeacher.js
+++ verify/validateTeacher.js
@@ -1,5 +1,5 @@
 const joi = require("@hapi/joi");
-const { idSubjectValidation } = require("./validateSubject");
+const { idSubjectValidation, updateSubjectValidation } = require("./validateSubject");
 
 //Validate data before CREATE TEACHER
 const createTeacherValidation = (data) => {
@@ -15,6 +15,21 @@ const createTeacherValidation = (data) => {
   return schema.validate(data);
 };
 
+//Validate data before UPDATE TEACHER
+const updateTeacherValidation = (data) => {
+  const schema = joi.object({
+    id: joi.string().required(),
+    name: joi.string(),
+    teacher_id: joi.string().min(1).max(10),
+    period_per_week: joi.number().integer().min(1).max(30),
+    grade: joi.number().integer().min(10).max(12),
+    subject: joi.string().min(1).max(10),
+    require: joi.string().length(10).pattern(new RegExp("^[0-1]*$")),
+  });
+
+  return schema.validate(data);
+};
+
 //Validate ID TEACHER
 const idTeacherValidation = (data) => {
   const schema = joi.object({
@@ -24,5 +39,6 @@ const idTeacherValidation = (data) => {
   return schema.validate(data);
 };
 
+module.exports.updateTeacherValidation = updateTeacherValidation;
 module.exports.idTeacherValidation = idTeacherValidation;
 module.exports.createTeacherValidation = createTeacherValidation;
